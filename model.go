f15947d3927408a508f0ace792f785dd57654ce4
chenyanqing26 <chenyanqing26@jd.com>
2023-09-22 16:44:37 +0800

    fix: 调整 model.go

--- model.go
+++ model.go
@@ -5,7 +5,7 @@ import "time"
 type Model struct {
 	Id        int        `json:"id" gorm:"column:id; AUTO_INCREMENT;"`
 	CreatedAt *time.Time `json:"createdAt" gorm:"column:created_at;type:datetime;not null;default:current_timestamp;comment:创建时间"`
-	CreatedBy uint64     `json:"createdBy" gorm:"column:created_by;type:bigint(20); unsigned;not null;default:0"`
+	CreatedBy int        `json:"createdBy" gorm:"column:created_by;type:int; unsigned;not null;default:0"`
 	UpdatedAt *time.Time `json:"updatedAt" gorm:"column:updated_at;type:datetime;not null;default:current_timestamp;autoUpdateTime;comment:更新时间"`
-	UpdatedBy uint64     `json:"updatedBy" gorm:"column:updated_by;type:bigint(20); unsigned;not null;default:0"`
+	UpdatedBy int        `json:"updatedBy" gorm:"column:updated_by;type:int; unsigned;not null;default:0"`
 }
